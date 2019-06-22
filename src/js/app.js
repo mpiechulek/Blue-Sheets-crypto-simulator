642b8101db5a9cf452effc72a50f58df22f115df
MP <47081376+mpiechulek@users.noreply.github.com>
2019-06-22 17:27:30 +0200

    RWD added

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+//jshint esversion:6
+import { getDate, getTime } from './date.js';
+
+//geting todays date and adding it to the header h2
+getDate();
+getTime();
---
+++ src/js/date.js
@@ -0,0 +1,16 @@
+//jshint esversion:6
+
+export const getDate = () => {
+    let today = new Date();
+    let dateOptions = { weekday: 'long', month: 'short', day: 'numeric' };
+    let todayDate = today.toLocaleString('en-US', dateOptions);
+    document.getElementById('date').innerHTML = todayDate;
+};
+
+export const getTime = () => {
+    let today = new Date();
+    let timeOptions = { hour: '2-digit', minute: '2-digit', second: 'numeric' };
+    let time = today.toLocaleString('pl-PL', timeOptions);
+    document.getElementById('time').innerHTML = time;
+    setTimeout(getTime, 500);
+};
